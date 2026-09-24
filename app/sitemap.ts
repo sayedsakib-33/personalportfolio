4e948dae3fb8289bd3ce4f013162794f7862ea57
Sayed Shadman Sakib <sayed.sakib2025@gmail.com>
2026-09-25 01:07:46 +0600

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from "next";
+export default function sitemap():MetadataRoute.Sitemap{return[{url:"https://personalportfolio.vercel.app",lastModified:new Date(),changeFrequency:"monthly",priority:1}];}
